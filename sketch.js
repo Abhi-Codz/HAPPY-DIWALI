b6ec5975ea8797526d9a9a6435a057f1e85f1564
Abhinavprogrammer <71148737+Abhinavprogrammer@users.noreply.github.com>
2020-11-12 04:34:25 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
+//Naming variables
 var diwaliDiya, diwaliAnimation;
+var soundFire;
 
 function preload()
 {
+    //Loading animation
     diwaliAnimation = loadAnimation("Diwali0.png", "Diwali1.png", "Diwali2.png", "Diwali3.png", "Diwali4.png", "Diwali5.png");
+    
+    //Loading sound
+    soundFire = loadSound("y2mate.com - FIREWORKS GREEN SCREEN AND SOUND EFFECTS.mp3");
 }
 
 
@@ -10,11 +16,13 @@ function setup()
 {
     createCanvas(540, 520);
 
+     //Looping the sound
+     soundFire.loop();
+
     //Creating sprite for diwali diya
     diwaliDiya = createSprite(250, 250);
     diwaliDiya.addAnimation("firing", diwaliAnimation);
     diwaliDiya.scale = 0.7;
-
 }
 
 
